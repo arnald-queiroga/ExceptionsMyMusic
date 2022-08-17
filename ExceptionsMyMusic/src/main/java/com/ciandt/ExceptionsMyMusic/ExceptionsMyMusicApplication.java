8fe9cc5272b668297e9a207fc34f802f3dc22416
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-08-17 15:58:24 -0300

    Project Created

---
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/ExceptionsMyMusicApplication.java
@@ -0,0 +1,13 @@
+package com.ciandt.ExceptionsMyMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsMyMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsMyMusicApplication.class, args);
+	}
+
+}
---
+++ ExceptionsMyMusic/src/test/java/com/ciandt/ExceptionsMyMusic/ExceptionsMyMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciandt.ExceptionsMyMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsMyMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
