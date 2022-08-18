31e74be4cd8abb248679796a91ae064d3c2ad139
jmmarao <jmmarao@ciandt.com>
2022-08-18 16:53:29 -0300

    [BOOT-706] Create Music and MusicDTO

---
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/entities/Music.java
@@ -0,0 +1,13 @@
+package com.ciandt.ExceptionsMyMusic.domain.entities;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Music {
+    private String id;
+    private String name;
+}
