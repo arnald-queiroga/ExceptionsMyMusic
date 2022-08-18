a8de12533a22b21aa9eadf6f1fd2c7f084e63050
alessandrasciandt <alessandras@ciandt.com>
2022-08-18 16:20:18 -0400

    [BOOT-780] creating new PLaylist and DTO class.

---
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/entities/Playlist.java
@@ -0,0 +1,14 @@
+package com.ciandt.ExceptionsMyMusic.domain.entities;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+
+public class Playlist {
+    private String id;
+
+}
