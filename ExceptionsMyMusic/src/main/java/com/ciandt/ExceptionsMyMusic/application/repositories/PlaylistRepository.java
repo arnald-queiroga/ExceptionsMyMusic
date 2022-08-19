3c8c4bce61a14b78f03952c471944844c05d1000
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-08-19 15:35:50 -0300

    [BOOT-748 e BOOT-797] Create Repositories: Artist, Music and Playlist

---
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/application/repositories/PlaylistRepository.java
@@ -0,0 +1,7 @@
+package com.ciandt.ExceptionsMyMusic.application.repositories;
+
+import com.ciandt.ExceptionsMyMusic.domain.entities.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistRepository extends JpaRepository <Playlist, String> {
+}
