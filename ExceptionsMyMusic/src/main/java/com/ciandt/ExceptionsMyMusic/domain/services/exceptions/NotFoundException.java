80e9f228aee6d48271136211902a179fa00089d6
Maikon Jakson <mjakson@ciandt.com>
2022-08-24 17:43:50 -0300

    [BOOT-799] Add PlaylistController

--- ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/services/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ciandt.ExceptionsMyMusic.domain.services.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NO_CONTENT)
-public class NotFoundException extends RuntimeException { }
