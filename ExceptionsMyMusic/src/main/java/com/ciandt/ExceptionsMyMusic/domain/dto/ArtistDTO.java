bae9f65bf43aad5ac81f1eb2e71c1d09d0527db9
alessandrasciandt <alessandras@ciandt.com>
2022-08-24 10:36:13 -0400

    [BOOT-753] Creating Service to find Musics by Artist Name, Controller and Repository

--- ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/dto/ArtistDTO.java
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/dto/ArtistDTO.java
@@ -1,25 +1,21 @@
 package com.ciandt.ExceptionsMyMusic.domain.dto;
 
 import com.ciandt.ExceptionsMyMusic.domain.entities.Artist;
-import com.ciandt.ExceptionsMyMusic.domain.entities.Music;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class MusicDTO {
+public class ArtistDTO {
     private String id;
     private String name;
-    private Artist artist;
 
-   public MusicDTO(Music entity) {
+    public ArtistDTO(Artist entity){
         this.id = entity.getId();
         this.name = entity.getName();
-        this.artist = entity.getArtist();
     }
 }
--- ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/entities/Artist.java
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/domain/entities/Artist.java
@@ -1,18 +1,20 @@
 package com.ciandt.ExceptionsMyMusic.domain.entities;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@NoArgsConstructor
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
 @Setter
+@Getter
+@EqualsAndHashCode
 @Entity
-@Table(name = "Musicas")
-public class Music {
+@Table(name = "Artistas")
+public class Artist {
 
     @Id
     @GeneratedValue(generator = "uuid")
@@ -23,10 +25,7 @@ public class Music {
     @Column(name = "Nome")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "ArtistaId", nullable = false)
-    private Artist artist;
-
-    @ManyToMany(mappedBy = "musics")
-    private Set<Playlist> playlists = new HashSet<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "artist")
+    private Set<Music> musics = new HashSet<>();
 }
